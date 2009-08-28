0a605d13ad53db4bfb3367c487a4e1e674edf692
arnaud.a.ramey <arnaud.a.ramey@cc2b286e-88d6-11de-9095-6b49e043a1b4>
2009-08-28 14:34:28 +0000

    shortcuts

--- Asker/VisualAsker.java
+++ Asker/VisualAsker.java
@@ -7,11 +7,15 @@ import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.ImageIcon;
+import javax.swing.InputMap;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
 
 import IO.LibUtils;
 import Lessons.VocParser.WordPicker;
@@ -71,8 +75,8 @@ public class VisualAsker extends JPanel {
 		else {
 			unknownWord_field.setText("");
 			unknownWord_field.setBackground(Color.white);
-			int w = Math.max(100, (int) (0.9f * unknownWord_field.getWidth()) );
-			int h = Math.max(100, (int) (0.9f * unknownWord_field.getHeight()) );
+			int w = Math.max(100, (int) (0.9f * unknownWord_field.getWidth()));
+			int h = Math.max(100, (int) (0.9f * unknownWord_field.getHeight()));
 			unknownWord_field.setIcon(new ImageIcon(currentQuestion.getImage(w,
 					h)));
 		}
@@ -93,11 +97,13 @@ public class VisualAsker extends JPanel {
 
 	/**
 	 * build the buttons to choose the type of exercice
-	 * @param lessons the name of the lessons
+	 * 
+	 * @param lessons
+	 *            the name of the lessons
 	 */
 	private void chooseLesson_makeButtons(String[] lessons) {
 		int ITEMS_PER_LINE = 3;
-		
+
 		/* create panel */
 		removeAll();
 		setLayout(new GridBagLayout());
@@ -113,8 +119,8 @@ public class VisualAsker extends JPanel {
 			final int i_bis = i;
 			// delete the number at the beginnings
 			String text = lessons[i];
-			text = text.substring(text.indexOf(" ")+1);
-			text = LibUtils.firstLetterUpperCase( text );
+			text = text.substring(text.indexOf(" ") + 1);
+			text = LibUtils.firstLetterUpperCase(text);
 			// create the button
 			JButton butt = new JButton(text);
 			butt.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
@@ -129,7 +135,7 @@ public class VisualAsker extends JPanel {
 			});
 			// add it to the panel
 			add(butt, c);
-			if ((i+1) % ITEMS_PER_LINE == 0) {
+			if ((i + 1) % ITEMS_PER_LINE == 0) {
 				c.gridx = 0;
 				c.gridy++;
 			} else {
@@ -158,7 +164,8 @@ public class VisualAsker extends JPanel {
 			return;
 		}
 
-		if (queue.getTypeOfQuestion() == QuestionQueue.VOCABULARY && queue.wp.areAllLessonsForbidden()) {
+		if (queue.getTypeOfQuestion() == QuestionQueue.VOCABULARY
+				&& queue.wp.areAllLessonsForbidden()) {
 			queue.wp.vocLesson_makeButtons(this);
 			return;
 		}
@@ -173,23 +180,36 @@ public class VisualAsker extends JPanel {
 		lessonTitle_field.setFont(new Font(Font.DIALOG, Font.ITALIC, 14));
 		lessonTitle_field.setHorizontalAlignment(JTextField.RIGHT);
 
-		knowButton = new JButton("I know");
-		knowButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		/* create actions */
+		Action knowAction = new AbstractAction() {
+			private static final long serialVersionUID = 1L;
+			public void actionPerformed(ActionEvent arg0) {
 				displayAnswer();
 				queue.declareQuestion_known();
 				displayQuestion();
 			}
-		});
-
-		doNotKnowButton = new JButton("???");
-		doNotKnowButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		};
+		Action unknowAction = new AbstractAction() {
+			private static final long serialVersionUID = 1L;
+			public void actionPerformed(ActionEvent arg0) {
 				displayAnswer();
 				queue.declareQuestion_unknown();
 				displayQuestion();
 			}
-		});
+		};
+		
+		/* set actions to buttons */
+		knowButton = new JButton(knowAction);
+		knowButton.getInputMap(JButton.WHEN_IN_FOCUSED_WINDOW).put(
+				KeyStroke.getKeyStroke("F1"), "know");
+		knowButton.getActionMap().put("know", knowAction);
+		knowButton.setText("I know.");
+		
+		doNotKnowButton = new JButton(unknowAction);
+		doNotKnowButton.getInputMap(JButton.WHEN_IN_FOCUSED_WINDOW).put(
+				KeyStroke.getKeyStroke("F2"), "unknow");
+		doNotKnowButton.getActionMap().put("unknow", unknowAction);
+		doNotKnowButton.setText("???");
 
 		/* layout */
 		removeAll();
@@ -223,7 +243,7 @@ public class VisualAsker extends JPanel {
 		displayQuestion();
 		validate();
 	}
-	
+
 	/**
 	 * @return the queue
 	 */
@@ -237,7 +257,7 @@ public class VisualAsker extends JPanel {
 		// System.out.println(wp.getRandomQuestion());
 
 		VisualAsker jp = new VisualAsker();
-		 jp.queue.setWordPicker(wp);
+		jp.queue.setWordPicker(wp);
 		// jp.queue.wp.listOfWords.displayAllWords();
 
 		// create window
